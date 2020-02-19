a3805048205a411caa6f73837190759d820fab88
Philippe Candille <philippe@oclock.io>
2020-02-19 11:39:51 +0100

    premier fichier de test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,12 +1,30 @@
-var assert = require('assert');
+// Import npm
+import { expect } from 'chai';
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-      assert.equal([1, 2, 3].indexOf(10), -1);
-      assert.equal([1, 2, 3].indexOf('a'), -1);
-      assert.equal([1, 2, 3].indexOf(5), -1);
+// Code
+const value = 2;
+
+function addition(a, b) {
+  return a + b;
+}
+
+// Tests
+
+// describe me permet de définir une série / un chapitre de tests
+describe('Tests de démo', () => {
+  describe('Série de tests : addition', () => {
+    // 1er test
+    it('expected to be a function', () => {
+      // l'assertion à vérifier
+      expect(addition).to.be.a('function');
     });
+
+    // 2eme test (dans la meme série)
+    it('expected to return sum of parameters', () => {
+      expect(addition(1, 2)).to.be.equal(3);
+    });
+
+    // status pending. Pas d'assertion à tester
+    it('should ... ');
   });
 });
--- tests/readme.md
+++ tests/readme.md
@@ -6,3 +6,14 @@ Mocha : https://mochajs.org/
 
 Mocha va permettre de lancer des suites de tests avec `describe` pour définir le sujet et `it` pour indiquer un test.
 
+## lib assertion
+
+Chai : https://www.chaijs.com/
+
+Syntaxe pour nos assertions. Syntaxe beaucoup plus lisible qu'à l'accoutumé.
+
+- should
+- expect
+- assert
+
+
